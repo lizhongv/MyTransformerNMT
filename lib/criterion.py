05a3d8a0c9f1c5c1537216bd8721f09feb047954
SIGMOID <1535899184@qq.com>
2022-12-22 21:20:18 +0800

    Update criterion.py

--- lib/criterion.py
+++ lib/criterion.py
@@ -5,25 +5,25 @@ from torch.autograd import Variable
 
 class LabelSmoothing(nn.Module):
     """
-    Implement label smoothing.
-
     LabelSmoothing 防止过于相信label，添加一个均分分布的噪声。
     """
     def __init__(self, size, padding_idx, smoothing=0.0):
         super(LabelSmoothing, self).__init__()
-        self.criterion = nn.KLDivLoss(size_average=False)
+        self.criterion = nn.KLDivLoss(reduction="sum")
         self.padding_idx = padding_idx
         self.confidence = 1.0 - smoothing
-        self.smoothing = smoothing
+        self.smoothing = smoothing  # 平滑系数e
         self.size = size
         self.true_dist = None
 
     def forward(self, x, target):
         assert x.size(1) == self.size
+       
         true_dist = x.data.clone()
-        true_dist.fill_(self.smoothing / (self.size - 2))  # e*u(k) 填充
-        true_dist.scatter_(1, target.data.unsqueeze(1), self.confidence)  # 赋值 (1-e)*q(y|x)
-        true_dist[:, self.padding_idx] = 0
+        true_dist.fill_(self.smoothing / (self.size - 2))  # 填充 e*u(k) 其中 u(k)=1/(size-2) 
+        true_dist.scatter_(1, target.data.unsqueeze(1), self.confidence)  # ground truth 分布 q(k), q'(k) = (1-e)*q(k) + e*u(k)
+        true_dist[:, self.padding_idx] = 0  
+      
         mask = torch.nonzero(target.data == self.padding_idx)
         if mask.dim() > 0:
             true_dist.index_fill_(0, mask.squeeze(), 0.0)
